385fc480209aa80d90f65923a542943794209e98
Beni <beniamin.silas@gmail.com>
2019-12-16 17:52:32 +0200

    New Calculator 4

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,16 +3,16 @@ public class Calculator {
 
     public static void main(String args[]) {
 
-        int x=0;
-        int y=0;
-        int op=0;
+        float x=0;
+        float y=0;
+        float op=0;
         char c='+';
         Scanner userInput= new Scanner(System.in);
         System.out.print("x = ");
-        x=userInput.nextInt();
+        x=userInput.nextFloat();
 
         System.out.print("y = ");
-        y=userInput.nextInt();
+        y=userInput.nextFloat();
         System.out.print("Selecteaza un operator = ");
         c=userInput.next().charAt(0);
 
