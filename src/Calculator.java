4688bb3bb824f39952eef43f851c1afd56c9dfe3
Beni <beniamin.silas@gmail.com>
2019-12-16 17:38:38 +0200

    New Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 public class Calculator {
+
     public static void main(String args[]) {
-int x=10;
-int y=5;
-int op=0;
-char c='%';
+
+        int x=0;
+        int y=0;
+        int op=0;
+        char c='+';
+        Scanner userInput= new Scanner(System.in);
+        x=userInput.nextInt();
+
+        y=userInput.nextInt();
+        c=userInput.next().charAt(0);
+
 
 
 if(c=='+')op=x+y;
